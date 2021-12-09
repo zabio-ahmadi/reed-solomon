8dc20beb5a657169f086d0b90d9185471764fc2b
Kurteshi <agnon.kurteshi@etu.hesge.ch>
2021-12-09 11:03:00 +0100

    euclide et inverse

--- euclide_etendu.py
+++ euclide_etendu.py
@@ -1,35 +1,58 @@
 #----------------------------------------------------------------------------
-# Created By  : Kurteshi, Ahamdi, Dymarzyck
+# Created By  : Kurteshi, Ahamadi, Dymarcyzk
 # Created Date: 06.12.2021
 # version ='1.0'
 # ---------------------------------------------------------------------------
 # Euclide étendu
 # ---------------------------------------------------------------------------
-# Imports Line 5
+# 
 # ---------------------------------------------------------------------------
 #
 import math
 def euclide_etendu(dividende, diviseur):
-    x = 1
-    v = 1
-    y = 0
-    u = 0
-    inverse_modulaire = diviseur
+    #initialisation
+    x = 1 #x0
+    v = 1 #y1
+    y = 0 #y0
+    u = 0 #x1
+    diviseur_pour_inverse = diviseur #permettra de calculer l'inverse modulaire
+    #euclide simple
     while diviseur != 0:
         quotient = dividende //diviseur
         reste = dividende % diviseur
-        m = u-quotient*x
-        n = v-quotient*y
         dividende = diviseur
         diviseur = reste
+    #-----------------------------------
+    #euclide etendu
+        m = u-quotient*x
+        n = v-quotient*y
         u = x
         v = y
         x = m
         y = n
+    #------------------------------------
     
-    return (u ,v, )
+    # return inverse modulaire
+    if diviseur != 0:
+        return (v ,u, "NULL")
+    else:
+        return (v ,u, v % diviseur_pour_inverse)
 
-print(euclide_etendu(108, 309))
+print(euclide_etendu(414235, 124124))
+
+def mod_inverse(x,m):
+    for n in range(m):
+        if (x * n) % m == 1:
+            return n
+            break
+
+        elif n == m - 1:
+            return "Null"
+        else:
+            continue
+
+print(mod_inverse(414235, 124124))
+
 
 
 
